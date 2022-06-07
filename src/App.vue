7e7c49f2fc3ae056e1a92c64847fe39195de4519
lenovo <radheshyamb9@gmail.com>
2022-06-07 23:26:36 +0530

    ADDED : conditionally render one thing or another. There's also v-else-if

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h3>Do you like tacos?</h3>
+
+    <input v-model="tacos" value="yes" type="radio" id="yes" />
+    <label for="yes"> yes</label><br />
+
+    <input v-model="tacos" value="no" type="radio" id="no" />
+    <label for="no"> no</label><br />
+
+    <p v-if="tacos === 'yes'" class="thumbs">👍</p>
+    <p v-else-if="tacos === 'no'">you're a monster</p>
+
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tacos: ''
+    }
+  }
+}
+</script>
